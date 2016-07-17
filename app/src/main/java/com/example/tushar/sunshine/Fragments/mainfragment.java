d04c5280009b07e1ec91e20f49628d6ec318220c
Tushar Mann <tusharmann29@gmail.com>
2016-07-17 18:30:57 +0530

    mainfragment added

---
+++ app/src/main/java/com/example/tushar/sunshine/Fragments/mainfragment.java
@@ -0,0 +1,23 @@
+package com.example.tushar.sunshine.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tushar.sunshine.R;
+
+public class mainfragment extends Fragment {
+
+    View v;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        v=inflater.inflate(R.layout.fragment_mainfragment, container, false);
+        return v;
+    }
+
+}
