24dae359d4b9bc19eaa5a5cd2b17b49925f331a8
Tushar Mann <tusharmann29@gmail.com>
2016-08-03 01:07:25 +0530

    scrolling is not done properly

--- app/src/main/java/com/example/tushar/sunshine/MainActivity.java
+++ app/src/main/java/com/example/tushar/sunshine/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.tushar.sunshine;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        CollapsingToolbarLayout collapsingToolbarLayout;
+        collapsingToolbarLayout=(CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
