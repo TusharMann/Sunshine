fe593f0a72e2c1afaf7b2070655ea6798353a84d
TusharMann <tusharmann29@gmail.com>
2016-12-23 15:33:55 +0530

    facebook login done

--- app/src/main/java/com/example/tushar/sunshine/MainActivity.java
+++ app/src/main/java/com/example/tushar/sunshine/MainActivity.java
@@ -3,27 +3,37 @@ package com.example.tushar.sunshine;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Toast;
 
-import com.example.tushar.sunshine.Fragments.mainfragment;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.share.model.ShareLinkContent;
+
+import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mainfragment fragment=new mainfragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.mainActivity_framelayout,fragment);
+//        mainfragment fragment=new mainfragment();
+//        getSupportFragmentManager().beginTransaction().replace(R.id.mainActivity_framelayout,fragment);
 
 
 
@@ -56,19 +66,57 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        callbackManager = CallbackManager.Factory.create();
+        final LoginButton loginButton = (LoginButton)findViewById(R.id.login_button_fb);
+        loginButton.setReadPermissions(Arrays.asList(
+                "email", "user_friends"));
+        ShareLinkContent content = new ShareLinkContent.Builder()
+                .setContentUrl(Uri.parse("https://developers.facebook.com"))
+                .build();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(MainActivity.this, loginResult.getAccessToken().getToken(), Toast.LENGTH_LONG)
+                        .show();
+                AccessToken accessToken = loginResult.getAccessToken();
+                GraphRequest request = GraphRequest.newMeRequest(
+                        accessToken,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                String email = object.optString("email");
+                                String uid = object.optString("id");
+                                Toast.makeText(MainActivity.this, email + " " + uid, Toast.LENGTH_LONG).show();
+
+                                // Application code
+                            }
+                        });
+            }
+
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("error", error.toString());
+                Toast.makeText(MainActivity.this,
+                        error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
 
 
         //((CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar)).setTitle("Coffee");
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 
